46d8dd16ae0c95724bcaf04c4059ba5120feb367
artemy22 <artem.nii@gmail.com>
2019-10-18 16:02:51 +0300

    #1 is done!

--- src/main/java/task91.java
+++ src/main/java/task91.java
@@ -1,8 +1,41 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+
 /*
 1. Создать аннотацию, которая принимает параметры для метода.
 Написать код, который вызовет метод, помеченный этой аннотацией, и передаст параметры аннотации в вызываемый метод.
  */
 
-public class task91 {
+@Target(value = ElementType.METHOD)
+@Retention(value = RetentionPolicy.RUNTIME)
+@interface Test {
+    int a();
 
+    int b();
+}
+
+class Multiply {
+    @Test(a = 123, b = 456)
+    static void myRes(int a, int b) {
+        System.out.println(a * b);
+    }
+}
+
+public class task91 {
+    public static void main(String[] args) {
+        try {
+            Class<?> cls = Multiply.class;
+            Method method = cls.getDeclaredMethod("myRes", int.class, int.class);
+            if (method.isAnnotationPresent(Test.class)) {
+
+                Test ma = method.getAnnotation(Test.class);
+                method.invoke(null, ma.a(), ma.b());
+            }
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
 }
