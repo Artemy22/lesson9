1d647c1b94daea734f1292de58f78f2296bf4b00
artemy22 <artem.nii@gmail.com>
2020-01-16 12:06:42 +0200

    Refactor Multiply method

--- src/main/java/task91.java
+++ src/main/java/task91.java
@@ -1,13 +1,14 @@
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import java.lang.reflect.Method;
 
 /*
 1. Создать аннотацию, которая принимает параметры для метода.
 Написать код, который вызовет метод, помеченный этой аннотацией, и передаст параметры аннотации в вызываемый метод.
  */
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
 
 @Target(value = ElementType.METHOD)
 @Retention(value = RetentionPolicy.RUNTIME)
@@ -15,24 +16,26 @@ import java.lang.reflect.Method;
     int a();
 
     int b();
+
+    int c();
 }
 
 class Multiply {
-    @Test(a = 123, b = 456)
-    static void myRes(int a, int b) {
-        System.out.println(a * b);
+    @Test(a = 3, b = 5, c = 12)
+    public static void test(int a, int b, int c) {
+        System.out.println("a * b * c = " + a * b * c);
     }
 }
 
-public class task91 {
+class AnnotTest {
     public static void main(String[] args) {
         try {
             Class<?> cls = Multiply.class;
-            Method method = cls.getDeclaredMethod("myRes", int.class, int.class);
+            Method method = cls.getMethod("test", int.class, int.class, int.class);
             if (method.isAnnotationPresent(Test.class)) {
 
                 Test ma = method.getAnnotation(Test.class);
-                method.invoke(null, ma.a(), ma.b());
+                method.invoke(null, ma.a(), ma.b(), ma.c());
             }
         } catch (Exception e) {
             System.out.println(e.toString());
